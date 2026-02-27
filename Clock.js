03a53d989cc7fecddf87281c6a29ab621286ee80
sarah-lorber <sarah.lorber@mymail.champlain.edu>
2026-02-27 17:44:21 -0500

    Update Clock.js

--- Clock.js
+++ Clock.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
+export class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = { date: new Date() };
   }
   render() {
-    return <div>{this.state.date.toLocaleTimeString()}</div>;
+    return (
+      <div>
+        {this.props.isPrecise
+          ? this.state.date.toISOString()
+          : this.state.date.toLocaleTimeString()}
+      </div>
+    );
+  }
+  startInterval(){
+let delay;
+    delay = this.props.isPrecise ? 100 : 1000 
+  this.intervalID = setInterval(() => {
+  this.setState({ date: new Date() });
+}, delay)
   }
   componentDidMount() {
-const oneSecond = 1000;
-setInterval(() => {
-  this.setState({ date: new Date() });
-}, oneSecond);
+    const oneSecond = 1000;
+    this.intervalID = this.startInterval()
+  }
+  componentDidUpdate(prevProps){
+    if (this.props.isPrecise === prevProps.isPrecise) {
+  return;
+}
+    clearInterval(this.intervalID);
+    this.startInterval();
+
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
   }
 }
-
-ReactDOM.render(<Clock />, document.getElementById('app'));
