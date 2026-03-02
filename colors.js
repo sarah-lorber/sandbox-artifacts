8fcc43c10171ddff88589ef44b1b5a6c64998419
sarah-lorber <sarah.lorber@mymail.champlain.edu>
2026-03-02 12:29:01 -0500

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,24 @@
+<! expand to all the colors!!!!!!>
+import React, { useState } from 'react';
+
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+
+export default function ColorPicker() {
+  const initialStateValue = 'Tomato';
+  const [color, setColor] = useState(initialStateValue);
+
+ const divStyle = {backgroundColor: color};
+
+  return (
+    <div style={divStyle}>
+      <p>Selected color: {color}</p>
+      {colorNames.map((colorName)=>(
+        <button 
+          onClick={() => setColor(colorName)} 
+          key={colorName}>
+       	     {colorName}
+      	</button>
+      ))}
+    </div>
+  );
+}
