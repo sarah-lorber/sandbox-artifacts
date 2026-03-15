85b215ac3340f3cc25bbd8b55b741a9a59e30a44
sarah-lorber <sarah.lorber@mymail.champlain.edu>
2026-03-15 16:28:53 -0400

    setup for transition

---
+++ bookiav2.ts
@@ -0,0 +1,48 @@
+var allData = [];
+var firstGo: boolean = true;
+
+addEventListener("keydown", (event) => { })
+onkeydown = (event) => { }
+      
+function goSearch(){
+  if (firstGo == true) {allData = getTheData();
+    firstGo = false;}
+  searchTerm: string = document.getElementById("searcher").value;
+	console.log(searchTerm);
+  searchIndex = searchTerm.length -1;
+  resultsfield = document.getElementById("resultsfield");
+  resultsfield.innerHTML = "";
+  for (var i = 0; i < allData.length; i++) {
+    report = allData[i];
+    if (i > -1){
+      resultsfield.innerHTML += "<p>Title: " + report.title + " (" + report.author + ")" + "</p>";
+      }}
+}
+
+// searchTerm is in report.title
+// if (where-object $_.title -ilike "[searchTerm]****") would return it
+// test() returns true/false (if true go)
+// make regex from searchTerm, regex.test(report.title)
+// how to use variable in regex?
+
+
+function getTheData(){
+return [
+   {
+    "title": "Wooden Star",
+    "author": "William Tenn"
+  },
+  {
+    "title": "Tomorrow Plus X",
+    "author": "Wilson Tucker"
+  },
+  {
+    "title": "Sorcery & Cecelia",
+    "author": "Wrede & Stevermer"
+  },
+  {
+    "title": "Pilgrimage",
+    "author": "Zenna Henderson"
+  }
+]
+}
