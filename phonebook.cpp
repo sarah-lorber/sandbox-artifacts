598ef6c4fc5ba5da1c85063d1c0c30746bddbc4f
sarah-lorber <sarah.lorber@mymail.champlain.edu>
2026-03-19 11:19:06 -0400

    fix and delete
    why isn't it matching???

---
+++ phonebook.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+struct PhoneRecord
+{
+    string name;
+    string number;
+};
+const string FILE_NAME = "phonerecords.txt";
+int main()
+{
+    ifstream fin(FILE_NAME);
+    if (!fin.is_open())
+    {
+        cout << FILE_NAME << " cannot be open!";
+        return 0;
+    }
+    //*******************************************
+    // TODO Part 1 - READING from file - ADDING to a vector
+    // 1- Read each phone record and create PhoneRecord object
+    // 2- Add the PhoneRecord object to vector
+    // 3- Repeat the above steps until you reach end of the file
+    // YOUR CODE PART 2 BEGINS
+    vector<PhoneRecord> yellow;
+    string line = "";
+    int i = 0;
+    while (!fin.eof())
+    {
+        PhoneRecord newRecord;
+        getline(fin, newRecord.name);
+        getline(fin, newRecord.number);
+        yellow.push_back(newRecord);
+        cout << newRecord.name << "\t" << newRecord.number << endl;
+        i++;
+    }
+    //*******************************************
+    // TODO Part 2 - SEARCHING in vector
+    // 1- Ask user a name via console,
+    // 2- Search that name in vector
+    // 3- if the name searched exists in the vector show the phone number
+    // 4- If not, display no record found with that name.
+    // YOUR CODE PART 2
+    string search;
+    bool found = false;
+    cout << "Who are you looking for: ";
+    cin >> search;
+
+    for (int i = 0; i < yellow.size(); i++) {
+        if (yellow[i].name == search) {
+            cout << yellow[i].number;
+            found = true;
+        };
+    }
+    if (found == false) { cout << "No match found." << endl; }
+
+    //*******************************************
+    // TODO Part 3 - OPTIONAL - UPDATING data in vector
+    // 1- Ask user a name via console,
+    // 2- Search that name in vector
+    // 3- If found update the phone number via new phone number entered via console
+    // YOUR CODE PART 3
+    cout << "Thank you!\n";
+}
